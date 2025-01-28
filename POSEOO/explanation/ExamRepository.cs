7d54378bccbd0033b1497a1133b6ea073b772dec
Fabian Baitura <peacemakerdude3@gmail.com>
2025-01-28 09:09:53 +0100

    adding explanation dbcontext set

---
+++ POSEOO/explanation/ExamRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace Persistence.Repositories;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Base.Persistence;
+
+using Core.Contracts;
+using Core.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+public class ExamRepository : GenericRepository<Exam>, IExamRepository
+{
+    public ExamRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+    public async Task<List<Exam>> GetAllExamnsIncludingQuestions()
+    {
+        return await Context.Set<Exam>().Include(exam => exam.ExamQuestions).ToListAsync();
+    }
+}
